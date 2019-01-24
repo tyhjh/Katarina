77d7c4866f8a800977e62d3ddb4017ce7eac14b6
Tyhj <tyhj@TyhjdeiMac.local>
2019-01-24 21:25:20 +0800

    更新.gitignore

--- app/src/main/java/com/yorhp/transcribescreen/module/DownloadFile.kt
+++
@@ -1,7 +0,0 @@
-package com.yorhp.transcribescreen.module
-
-/**
- * Created by Tyhj on 2017/10/28.
- */
-class DownloadFile(var url: String, var path: String) {
-}
--- app/src/main/java/com/yorhp/transcribescreen/module/UserInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.yorhp.transcribescreen.module
-
-/**
- * Created by Tyhj on 2017/10/25.
- */
-class UserInfo(var userName: String, var appVersion:String,var model: String, var androidVersion: String,var position:String,var imei:String) {
-
-}
